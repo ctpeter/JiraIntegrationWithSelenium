6621a54e46dd6af3e6914f7b5cf3074db860e437
ctpeter <88034362+ctpeter@users.noreply.github.com>
2022-02-26 22:02:42 +0800

    Update BaseTest.java

--- src/test/java/com/MyTests/BaseTest.java
+++ src/test/java/com/MyTests/BaseTest.java
@@ -5,6 +5,7 @@ package com.MyTests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
